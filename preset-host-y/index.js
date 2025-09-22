57afb106524a4c0b8572c052c7e557ffdc28dc03
Phil Scott <philscott.dev@gmail.com>
2025-09-22 04:52:43 -0400

    feat: condense to single preset

--- preset-host-y/index.js
+++
@@ -1,9 +0,0 @@
-// Map RN versions → capabilities → { name, version }
-// Add anything you want to pin (react-native, react, core libs, etc.)
-module.exports = {
-  "*": {
-    core: { name: "react-native", version: "^0.80.0" },
-    react: { name: "react", version: "^19.0.0" }
-    // add gesture-handler, reanimated, etc. as custom capabilities if you like
-  }
-}
